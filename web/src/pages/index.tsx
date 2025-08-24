20415b11a7906bf924223d185331ed2a4e12270a
benmneb <62878664+benmneb@users.noreply.github.com>
2025-08-24 13:13:48 +0800

    feat(web): add home page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,37 +1,45 @@
 import type { ReactNode } from 'react'
-import clsx from 'clsx'
 import Link from '@docusaurus/Link'
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
-import Layout from '@theme/Layout'
-import Heading from '@theme/Heading'
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext()
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/forward">
-            Read online now
-          </Link>
-        </div>
-      </div>
-    </header>
-  )
-}
 
 export default function Home(): ReactNode {
-  const { siteConfig } = useDocusaurusContext()
   return (
-    <Layout title={siteConfig.title} description={siteConfig.tagline}>
-      <HomepageHeader />
-      <main>
-        <p>Main!</p>
-      </main>
-    </Layout>
+    <main className="home">
+      <figure>
+        <img
+          src={require('@site/static/img/cover-lg.png').default}
+          alt="The Word of the Buddha cover designed in Hong Kong by Pressroom Printer and Designer Ltd"
+        />
+      </figure>
+      <section>
+        <hgroup>
+          <h1>The Word of the Buddha</h1>
+          <h2>
+            An Outline of the Teachings of the Buddha
+            <br />
+            in the Words of the Pāli Canon
+          </h2>
+          <p className="font-small-caps">Second Edition</p>
+          <p>
+            Compilation based on Ñāṇatiloka Mahāthera’s booklet
+            <br />
+            <em>The Word of the Buddha</em>
+          </p>
+          <p>
+            Updated with translations from Bhikkhu Bodhi
+            <br />
+            Edited by Ajahn Brahm
+          </p>
+        </hgroup>
+        <nav>
+          <Link className="button button--primary button--lg" to="/forward">
+            Read online
+          </Link>
+          <div className="button-group button-group--block">
+            <Link className="button button--secondary">Get EPUB</Link>
+            <Link className="button button--secondary">Get PDF</Link>
+          </div>
+        </nav>
+      </section>
+    </main>
   )
 }
