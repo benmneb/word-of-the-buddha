f3b3a829a4afe445cb387c5d9076755b8b44c94d
benmneb <62878664+benmneb@users.noreply.github.com>
2025-08-28 14:06:53 +0800

    fix(web): move github link from header to footer and be more specific with the codes license

--- web/docusaurus.config.ts
+++ web/docusaurus.config.ts
@@ -97,12 +97,6 @@ const config: Config = {
           ],
         },
         {
-          href: 'https://github.com/benmneb/word-of-the-buddha',
-          className: 'header-github-link',
-          'aria-label': 'GitHub repository',
-          position: 'right',
-        },
-        {
           type: 'search',
           position: 'right',
         },
@@ -119,10 +113,22 @@ const config: Config = {
     },
     footer: {
       style: 'light',
-      copyright: `Copyright © ${new Date().getFullYear()} Ajahn Brahmavaṃso.
-      All rights reserved.
-      Permission to copy, reprint and distribute this publication is hereby given provided no changes are made to the content or layout.
-      Reproduction in any way for commercial gain is prohibited.`,
+      copyright: `
+      <p>
+        Copyright © ${new Date().getFullYear()} Ajahn Brahmavaṃso.
+        All rights reserved.
+        Permission to copy, reprint and distribute this publication is hereby given provided no changes are made to the content or layout.
+        Reproduction in any way for commercial gain is prohibited.
+      </p>
+      <div>
+        Website by <a href="https://www.github.com/benmneb/word-of-the-buddha" target="_blank">benmneb</a>
+        <svg width="16px" height="16px" viewBox="-10 -5 1034 1034" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
+          <path fill="currentColor" d="M500 177q-115 0 -214 58q-96 57 -153 153q-58 99 -58 214t58 214q57 96 153 153q99 58 214 58t214 -58q96 -57 153 -153q58 -99 58 -214t-58 -214q-57 -96 -153 -153q-99 -58 -214 -58zM496 264h3h1q92 0 171 46q76 45 121 121q46 79 46 171t-46 171q-45 76 -121 121
+          q-79 46 -171 46t-171 -46q-76 -45 -121 -121q-46 -79 -46 -170.5t45 -170.5q44 -76 120 -121q77 -46 169 -47zM497 363q-56 0 -104.5 24t-82 66.5t-45.5 94.5h114q15 -34 47 -55t71 -21q35 0 65 17.5t47.5 47.5t17.5 65t-17.5 65t-47.5 47.5t-65 17.5q-39 0 -71 -21t-47 -54
+          h-114q12 52 45.5 94t82 66t104.5 24q64 0 119 -32.5t87 -87t32 -119.5t-32 -119.5t-87 -87t-119 -32.5z" />
+        </svg>
+        <a rel="license" href="https://www.gnu.org/licenses/agpl-3.0.txt" target="_blank">AGPL-3.0-or-later</a>
+      </div>`,
     },
   } satisfies Preset.ThemeConfig,
 
